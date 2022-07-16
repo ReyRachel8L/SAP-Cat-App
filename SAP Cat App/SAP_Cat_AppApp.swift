1a14295a80aff2ab851d5fdaf779cd63e41ecae9
ReyRachel8L <rlim.rachel08@gmail.com>
2022-07-16 10:54:09 +0800

    Initial Commit

---
+++ SAP Cat App/SAP_Cat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_Cat_AppApp.swift
+//  SAP Cat App
+//
+//  Created by LIM YEE SIN RACHEL stu on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_Cat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
